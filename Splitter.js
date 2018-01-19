a77103284cda2f11128bc08b2ba358d24309b40c
Mark Ritterhoff <mritterhoff@gmail.com>
2018-01-19 11:16:23 -0500

    general refactoring, started to add tax/tip

--- Splitter.js
+++ Splitter.js
@@ -3,12 +3,6 @@ import React from 'react'
 import {CellToggle} from './CellToggle.js'
 import {StringInput, PriceInput} from './Inputs.js'
 
-
-function getAsPriceString(num) {
-  return '$ ' + num.toFixed(2); 
-}
-
-
 class Splitter extends React.Component {
   constructor(props) {
     super(props);
@@ -52,24 +46,34 @@ class Splitter extends React.Component {
     });
   }
 
-  checkOrderPersonDish(pInd, dInd) {
+  didPersonOrderDish(pInd, dInd) {
     return this.state.orders[dInd][pInd];
   }
 
   getPeoplePerDish(dInd) {
     return this.state.people
-      .map((p, pInd) => (this.checkOrderPersonDish(pInd, dInd)))
+      .map((p, pInd) => (this.didPersonOrderDish(pInd, dInd)))
       .reduce((p, c) => p + c);
+  }
+
+  personCostForDish(pInd, dInd) {
+    return this.didPersonOrderDish(pInd, dInd)
+      ? this.state.dishes[dInd].price / this.getPeoplePerDish(dInd)
+      : 0;
+  }
+
+  // For each dish, 
+  getTotalForPerson(pInd) {
+    return this.state.dishes.map((dish, dInd) => (this.personCostForDish(pInd, dInd)), this)
+      .reduce((p, c) => p + c, 0);
   }
   
   addPerson(name) {
-    // index of new person
     let pInd = this.state.people.length;
     name = name || `Person ${pInd + 1}`
     console.log('adding person...');
 
     this.setState((prevState) => {
-      //let newOrders = Object.assign({}, prevState.orders);
       let newOrders = clone2D(prevState.orders);
 
       for (let row = 0; row < newOrders.length; row++) {
@@ -99,13 +103,13 @@ class Splitter extends React.Component {
     dish = dish || new Dish(`Dish ${dInd + 1}`, 0);
     console.log('adding dish...');
     this.setState((prevState) => {
-
-      let newOrders = clone2D(prevState.orders);
+      // let newOrders = clone2D(prevState.orders)
+      //   .concat(Array(peopleCount).fill(true));
       
       // add to the end (-1), don't delete any (0)
-      // add true for every person
+      // splice isn't chainable. unsure if I can switch to concat (above)
+      let newOrders = clone2D(prevState.orders);
       newOrders.splice(-1, 0, Array(peopleCount).fill(true));
-      console.log(newOrders);
 
       return {
         dishes: [...prevState.dishes, dish],
@@ -132,7 +136,9 @@ class Splitter extends React.Component {
 
             <div className="tbody">
               {this.getOrderRows()}
-              {this.getTotalRow(this.state.people)}
+              {this.getTaxRow()}
+              {this.getTipRow()}
+              {this.getTotalRow()}
             </div>
           </div>
         </div>
@@ -175,42 +181,57 @@ class Splitter extends React.Component {
     );
   }
 
-    // todo this should return a number
-  getTotalForPerson(pInd) {
-    return this.state.dishes.map((dish, dInd) => {
-      // did this person have the dish?
-      if (this.checkOrderPersonDish(pInd, dInd)) {
-        return dish.price / this.getPeoplePerDish(dInd);
-      }
-      return 0;
-    }, this)
-    .reduce((p, c) => p + c, 0);
-  }
-
   getTotalRow() {
-    let rowElements = [
-      <span>Total:</span>
-    ];
-    this.state.people.forEach((person, pInd) => {
-      rowElements.push(
-        <span style={{fontWeight: 'bold'}}>
-          {getAsPriceString(this.getTotalForPerson(pInd))}
-        </span>);
-    });
+    let rowEls = [<span>Total:</span>];
+    rowEls = rowEls.concat(this.state.people.map((person, pInd) => (
+      <span style={{fontWeight: 'bold'}}>
+        {priceAsString(this.getTotalForPerson(pInd))}
+      </span>
+    )));
 
     // put each row element in a _td, then put all of those in a _tr
     return (
       <_tr>
-        {rowElements.map((el, el_i) => <_td key={el_i}>{el}</_td>)}
+        {rowEls.map((el, el_i) => <_td key={el_i}>{el}</_td>)}
+      </_tr>
+    );
+  }
+
+  getTaxRow() {
+    let rowEls = [<span>Tax:</span>];
+    rowEls = rowEls.concat(this.state.people.map((person, pInd) => (
+      <span>
+        Tax
+      </span>
+    )));
+
+    // put each row element in a _td, then put all of those in a _tr
+    return (
+      <_tr>
+        {rowEls.map((el, el_i) => <_td key={el_i}>{el}</_td>)}
+      </_tr>
+    );
+  }
+
+  getTipRow() {
+    let rowEls = [<span>Tip:</span>];
+    rowEls = rowEls.concat(this.state.people.map((person, pInd) => (
+      <span>
+        Tip
+      </span>
+    )));
+
+    // put each row element in a _td, then put all of those in a _tr
+    return (
+      <_tr>
+        {rowEls.map((el, el_i) => <_td key={el_i}>{el}</_td>)}
       </_tr>
     );
   }
 
   getOrderRows() {
-    let people = this.state.people;
-    let dishes = this.state.dishes;
-
     let that = this;
+
     let getToggleCB = function (pInd, dInd) {
       return (setUnset) => {
         that.indicateOrder(setUnset, pInd, dInd);
@@ -232,22 +253,11 @@ class Splitter extends React.Component {
 
     // If the person ordered the dish, do some math. Else return 0.
     function getPrice(pInd, dInd) {
-      return getAsPriceString(this.checkOrderPersonDish(pInd, dInd)
-        ? this.state.dishes[dInd].price / this.getPeoplePerDish(dInd)
-        : 0);
+      return priceAsString(this.personCostForDish(pInd, dInd))
     }
 
-    return dishes.map((dish, dInd) => {
-      let rowElements = people.map((el, pInd) => (
-        <CellToggle 
-          enabled={that.checkOrderPersonDish(pInd, dInd)}
-          callback={getToggleCB(pInd, dInd)}
-          price={getPrice.bind(that)(pInd, dInd)}
-        />
-      ));
-
-      // put in the beggining
-      rowElements.splice(0, 0, 
+    return this.state.dishes.map((dish, dInd) => {
+      let rowEls = [
         <div>
           <StringInput 
             style={{float: 'left'}}
@@ -258,12 +268,20 @@ class Splitter extends React.Component {
             initalValue = {dish.price}
             onBlurCB = {getPriceCB(dInd)} />
         </div>
-      );
+      ];
+
+      rowEls = rowEls.concat(this.state.people.map((el, pInd) => (
+        <CellToggle 
+          enabled={that.didPersonOrderDish(pInd, dInd)}
+          callback={getToggleCB(pInd, dInd)}
+          price={getPrice.bind(that)(pInd, dInd)}
+        />
+      )));
 
       // put each row element in a _td, then put all of those in a _tr
       return (
         <_tr key={dInd}>
-          {rowElements.map((el, el_i) => <_td key={el_i}>{el}</_td>)}
+          {rowEls.map((el, el_i) => <_td key={el_i}>{el}</_td>)}
         </_tr>
       );
     });
@@ -285,19 +303,17 @@ function _tr(props) {
 }
 
 function getNamesHeader(people) {
-  // row starts with one empty cell
-  let rowElements = [<div/>];
-  people.forEach((el) => {
-    rowElements.push(
-      <StringInput 
-        initalValue = {el}
-        placeholder = 'Person'
-      />);
-  });
+  let rowEls = [<div/>];
+  rowEls = rowEls.concat(people.map((el) => (
+    <StringInput 
+      initalValue = {el}
+      placeholder = 'Person'
+    />
+  )));
 
   return (
     <div className="thead"> 
-      {rowElements.map((el, el_i) => <_th key={el_i}>{el}</_th>)}
+      {rowEls.map((el, i) => <_th key={i}>{el}</_th>)}
     </div>
   );
 }
@@ -311,6 +327,10 @@ class Dish {
 
 function clone2D(a) {
   return a.map(o => [...o]);
+}
+
+function priceAsString(num) {
+  return '$ ' + num.toFixed(2); 
 }
 
 export default Splitter;
